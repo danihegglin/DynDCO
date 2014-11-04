a205582591a620aa209bd5d6f09f84e01b785e40
Daniel <dani.hegglin@gmail.com>
2014-11-04 01:33:18 +0100

    maxsum ruff

--- src/main/scala/ch/uzh/dyndco/algorithms/dyndco/incomplete/maxsum/MaxSum.scala
+++ src/main/scala/ch/uzh/dyndco/algorithms/dyndco/incomplete/maxsum/MaxSum.scala
@@ -9,21 +9,23 @@ object MaxSum extends App {
    * Configuration
    */
   var timeslots : Int = 5
+  println("Configuration")
   
 	// initialize bipartite graph
-//  val graph = GraphBuilder.withConsole(true,8091).build
-    val graph = GraphBuilder.build
+  val graph = GraphBuilder.withConsole(true,8091).build
+  println("Graph initialized")
   
-//  // constraints
-//  var hard1 : Set[Int] = Set(1,2,3)
-//  var soft1 : Set[Int] = Set(5)
-//  var preference1 : Set[Int] = Set(4)
-//  var constraints1 = new Constraints(null,hard1,soft1,preference1)
-//  var hard2: Set[Int] = Set(1)
-//  var soft2: Set[Int] = Set(2,3,4)
-//  var preference2 : Set[Int] = Set(5)
-//  var constraints2 = new Constraints(null,hard2,soft2,preference2)
-//  
+  // constraints
+  var hard1 : Set[Int] = Set(1,2,3)
+  var soft1 : Set[Int] = Set(5)
+  var preference1 : Set[Int] = Set(4)
+  var constraints1 = new Constraints(null,hard1,soft1,preference1)
+  var hard2: Set[Int] = Set(1)
+  var soft2: Set[Int] = Set(2,3,4)
+  var preference2 : Set[Int] = Set(5)
+  var constraints2 = new Constraints(null,hard2,soft2,preference2)
+  println("Build constraints")
+  
 //  // vertices
 //  graph.addVertex(new VariableVertex(1,constraints1,timeslots))
 //  graph.addVertex(new VariableVertex(2,constraints2,timeslots))
