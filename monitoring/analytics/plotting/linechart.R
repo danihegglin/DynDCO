9d98c3a7ec22c11cc99cbea34ccfdd686e753b67
Daniel Hegglin <dani.hegglin@gmail.com>
2014-12-18 14:59:34 +0100

    linechart

--- monitoring/analytics/plotting/linechart.R
+++ monitoring/analytics/plotting/linechart.R
@@ -1,3 +1,9 @@
 # Create Line Chart
 experiment <- read.csv("~/git/dyndco/monitoring/analytics/results/combined.txt", sep=";")
 plot(experiment$timestamp, experiment$utility, xlab= "timepoint", ylab= "Utility", type='l', col='red') 
+
+experiment2 <- read.csv("~/Desktop/scalability_m.txt", sep=";")
+plot(experiment2$number, experiment2$time, xlab= "number", ylab= "time", type='l', col='red')
+
+experiment3 <- read.csv("~/Desktop/scalability_a.txt", sep=";")
+plot(experiment3$number, experiment3$time, xlab= "number", ylab= "time", type='l', col='red')
